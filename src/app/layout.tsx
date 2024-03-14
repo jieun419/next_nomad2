4eb5849ff872796c5fdf360e936ac8dd1c419d1b
jieun419 <crg1050@naver.com>
2024-03-15 00:41:33 +0900

    style: 불필요한 코드 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "../../components/navigation";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <body className={inter.className}>
+      <body>
         <Navigation />
         {children}
       </body>
