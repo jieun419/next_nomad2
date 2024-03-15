ce89269d5ad4fcc0f5443010fc5bec1ffc77ca9c
jieun419 <crg1050@naver.com>
2024-03-16 02:20:37 +0900

    feat: 메타 데이터 작성하기

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home | Next Movies",
+};
+
 export default function Home() {
   return (<h1>Hello</h1>);
 }
--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About us | Next Movies",
+};
+
+
 const AboutUs = () => {
   return (<h1>About us!</h1>);
 };
