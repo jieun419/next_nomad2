992b5e82bb73c0cf5a4fef04c27ffde0c4262036
jieun419 <crg1050@naver.com>
2024-04-01 22:42:22 +0900

    feat: movie info 스타일링 및 코드 수정

--- src/app/components/movie-info.tsx
+++ src/app/components/movie-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "@/app/(home)/page";
+import styles from "../styles/movie.info.module.css";
 
 const getMovise = async (id: string) => {
   console.log(`Fetching movies: ${Date.now()}`)
@@ -10,7 +11,15 @@ const getMovise = async (id: string) => {
 const MovieInfo = async ({ id }: { id: string }) => {
   const movie = await getMovise(id);
   return (
-    <h6>{JSON.stringify(movie)}</h6>
+    <div className={styles.container}>
+      <img src={movie.poster_path} alt={movie.title} className={styles.poster} />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target="_blank">Homepage →</a>
+      </div>
+    </div>
   );
 };
 
