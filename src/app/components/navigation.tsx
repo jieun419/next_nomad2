11c580bde5a8dc696de24caf4af94c913f8622c2
jieun419 <crg1050@naver.com>
2024-04-01 20:51:20 +0900

    design: navigation 스타일

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -2,12 +2,13 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import styles from '../styles/navigation.module.css';
 
 const Navigation = () => {
   const path = usePathname();
 
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li><Link href="/">Home {path === '/' ? '✨' : ''}</Link></li>
         <li><Link href="/about-us">About Us {path === '/about-us' ? '✨' : ''}</Link></li>
