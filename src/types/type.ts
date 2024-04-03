6bbbeb0688a5a77c68fd9d630568deea4d9e4851
jieun419 <crg1050@naver.com>
2024-04-04 03:56:46 +0900

    feat: type 파일 생성

---
+++ src/types/type.ts
@@ -0,0 +1,34 @@
+type GenreTdsT = {
+  genre_ids: Number;
+};
+
+export interface ImoviesT {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: GenreTdsT[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface IvideosT {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: true;
+  published_at: string;
+  id: string;
+  title: string;
+}
