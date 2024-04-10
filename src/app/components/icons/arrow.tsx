dbfaa8053d1c4aef0d6de06268e07192ff20ab71
jieun419 <crg1050@naver.com>
2024-04-11 00:54:44 +0900

    feat: arrow 아이콘 동적으로 방향 바꿀 수 있도록 props 설정

--- src/app/components/icons/arrow.tsx
+++ src/app/components/icons/arrow.tsx
@@ -1,7 +1,7 @@
 const Arrow = ({ type }: { type: string }) => {
   return (
-    <svg width="51" height="86" viewBox="0 0 51 86" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d={type === 'left' ? "M47 4L8 43L47 82" : "M4 82L43 43L4.00001 4"} stroke="white" stroke-width="10" />
+    <svg width="20" height="55" viewBox="0 0 51 86" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d={type === 'left' ? "M47 4L8 43L47 82" : "M4 82L43 43L4.00001 4"} stroke="white" strokeWidth="10" />
     </svg>
 
   );
