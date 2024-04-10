e462d89af075baaea28bc68acd38fcf418d21471
jieun419 <crg1050@naver.com>
2024-04-10 20:23:25 +0900

    feat: API 오타 수정 및 추가

--- src/app/services/movieApi.ts
+++ src/app/services/movieApi.ts
@@ -6,9 +6,33 @@ export const getVideos = async (id: string) => {
   return res.json();
 };
 
-export const getMovise = async (id: string) => {
+export const getAllMovies = async () => {
+  await new Promise((res) => setTimeout(res, 1000));
+  const res = await fetch(`${config.moviesApi}`);
+  return res.json();
+};
+
+export const getMovies = async (id: string) => {
   console.log(`Fetching movies: ${Date.now()}`);
   await new Promise((res) => setTimeout(res, 5000));
   const res = await fetch(`${config.moviesApi}/${id}`);
+  return res.json();
+};
+
+export const getCredits = async (id: string) => {
+  console.log(`Fetching credits: ${Date.now()}`);
+  const res = await fetch(`${config.moviesApi}/${id}/credits`);
+  return res.json();
+};
+
+export const getProviders = async (id: string) => {
+  console.log(`Fetching providers: ${Date.now()}`);
+  const res = await fetch(`${config.moviesApi}/${id}/providers`);
+  return res.json();
+};
+
+export const getSimilar = async (id: string) => {
+  console.log(`Fetching similar: ${Date.now()}`);
+  const res = await fetch(`${config.moviesApi}/${id}/similar`);
   return res.json();
 };
