a6103df5d65043fc037b50b98ba9f152b2a2413b
jieun419 <crg1050@naver.com>
2024-04-10 20:23:45 +0900

    feat: 스타일 수정 및 컴포넌트 분리

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,26 +2,30 @@ import { Metadata } from "next";
 import Movie from "../components/movie";
 import styles from "../styles/home.module.css";
 import { ImoviesT } from "@/types/type";
-import { config } from "@/config";
+import { getAllMovies } from "@/app/services/movieApi";
+import MovieCarousel from "../components/movie-carousel";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
-const getMovies = async () => {
-  await new Promise((res) => setTimeout(res, 1000));
-  const res = await fetch(config.moviesApi);
-  const json = await res.json();
-  return json;
-};
-
 export default async function HomePage() {
-  const movies = await getMovies();
+  const movies = await getAllMovies();
   return (
-    <div className={styles.container}>
-      {movies.map((movie: ImoviesT) => (
-        <Movie key={movie.id} id={movie.id} title={movie.title} poster_path={movie.poster_path} />
-      ))}
-    </div>
+    <>
+      <section className={styles.movieListContainer}>
+        <h2>인기 영화</h2>
+        <MovieCarousel movies={movies} />
+      </section>
+
+      <section className={styles.movieListContainer}>
+        <h2>상영중인 영화</h2>
+        <div className={styles.movieListWrap}>
+          {movies.map((movie: ImoviesT) => (
+            <Movie key={movie.id} id={movie.id} title={movie.title} poster_path={movie.poster_path} />
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
