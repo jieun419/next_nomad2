49fe11f0f60f7ceb60a3dfdd459995abde13b89a
jieun419 <crg1050@naver.com>
2024-04-11 00:53:56 +0900

    feat: 시맨틱 태그 및 스타일 수정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -12,20 +12,20 @@ export const metadata: Metadata = {
 export default async function HomePage() {
   const movies = await getAllMovies();
   return (
-    <>
+    <article className={styles.container}>
       <section className={styles.movieListContainer}>
-        <h2>인기 영화</h2>
+        <h3 className={styles.title}>인기 영화 TOP 10</h3>
         <MovieCarousel movies={movies} />
       </section>
 
       <section className={styles.movieListContainer}>
-        <h2>상영중인 영화</h2>
+        <h3 className={styles.title}>상영중인 영화</h3>
         <div className={styles.movieListWrap}>
           {movies.map((movie: ImoviesT) => (
             <Movie key={movie.id} id={movie.id} title={movie.title} poster_path={movie.poster_path} />
           ))}
         </div>
       </section>
-    </>
+    </article>
   );
 }
